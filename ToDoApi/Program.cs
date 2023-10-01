87bb3bb7632e85e7c6244e8221548faeb29c1506
Juarez Goulart <goulartgarcia12@gmail.com>
2023-10-01 20:56:59 -0300

    Adding secret parameter

--- ToDoApi/Program.cs
+++ ToDoApi/Program.cs
@@ -6,31 +6,37 @@ builder.Services.AddDbContext<ToDoDb>(opt => opt.UseInMemoryDatabase("TodoList")
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
-app.MapGet("/todoItems", async (ToDoDb db) => await db.ToDos.ToListAsync());
+app.MapGet("/todoItems", async (ToDoDb db) => await db.ToDos.Select(x => new ToDoItemDTO(x)).ToListAsync());
 
 app.MapGet("/todoItems/complete", async (ToDoDb db) => await db.ToDos.Where(t => t.IsComplete).ToListAsync());
 
 app.MapGet("/todoItems/{id}", async (int id, ToDoDb db) => await db.ToDos.FindAsync(id) 
     is ToDo todo 
-    ? Results.Ok(todo) 
+    ? Results.Ok(new ToDoItemDTO(todo)) 
     : Results.NotFound());
 
-app.MapPost("/todoItems", async (ToDo todo, ToDoDb db) =>
+app.MapPost("/todoItems", async (ToDoItemDTO toDoItemDto, ToDoDb db) =>
 {
-    db.ToDos.Add(todo);
+    var todoItem = new ToDo()
+    {
+        IsComplete = toDoItemDto.IsComplete,
+        Name = toDoItemDto.Name
+    };
+    
+    db.ToDos.Add(todoItem);
     await db.SaveChangesAsync();
 
-    return Results.Created($"/todoItems/{todo.Id}", todo);
+    return Results.Created($"/todoItems/{todoItem.Id}", new ToDoItemDTO(todoItem));
 });
 
-app.MapPut("/todoItems/{id}", async (int id, ToDo inputTodo, ToDoDb db) =>
+app.MapPut("/todoItems/{id}", async (int id, ToDoItemDTO toDoItemDto, ToDoDb db) =>
 {
     var todo = await db.ToDos.FindAsync(id);
 
     if (todo is null) return Results.NotFound();
 
-    todo.Name = inputTodo.Name;
-    todo.IsComplete = inputTodo.IsComplete;
+    todo.Name = toDoItemDto.Name;
+    todo.IsComplete = toDoItemDto.IsComplete;
 
     await db.SaveChangesAsync();
 
